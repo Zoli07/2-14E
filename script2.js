fb6e0ec7f83fd5c9234a57449de99b03c29dc0f3
Borbély Zoltán <borbelyz@kkszki.hu>
2022-09-15 18:30:36 +0200

    js2 commit

--- script2.js
+++ script2.js
@@ -1,5 +1,24 @@
-let i=0;
+/*let i=0;
 while (i<3) {
     console.log(i);
-    i+1;
+    i++;
 }
+
+let i=3;
+while (i) {
+    console.log(i);
+    i--;
+}
+
+let i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<3);*/
+
+for(let i=0;i<3;i++){
+    console.log(i);
+    document.write("<br>Hello</br>")
+}
+i=2;
+console.log(i);
