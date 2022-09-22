0a5a0ecbe5713cffb70b889ec74c27a235fca832
Borbély Zoltán <borbelyz@kkszki.hu>
2022-09-22 17:27:34 +0200

    óra1

---
+++ function.js
@@ -0,0 +1,16 @@
+/*function Hello(){
+    console.log("hello");
+}
+let func=Hello;
+func();
+Hello();
+*/
+let hello=function(){
+    console.log("hello");
+}
+hello();
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(2,3));
--- script2.js
+++ script2.js
@@ -21,6 +21,29 @@ for(let i=0;i<3;i++){
     document.write("<br>Hello</br>")
 }
 i=2;
-console.log(i);*/
+console.log(i);
 
-let p=prompt("ADj meg egy számot! ", "")
+let p=prompt("Adj meg egy számot! ", "");
+console.log(p);
+p=Number(p);
+console.log(typeof(p));
+let q=5;
+console.log(p+q);
+console.log(typeof(p+q));
+
+let j=prompt("Kérlek add meg a j értékét!");
+j=Number(j);
+while(j){
+    console.log(j);
+    j--;
+}
+alert("itt a vége , j értéke: "+j);
+j++;
+alert("j értéke: "+j);
+*/
+let j=prompt("Kérem az életkorát!",10);
+let k=(j<3) ? alert("bébi") :
+    (j<10) ? "gyerek" :
+    (j<18) ? "fiatal felnőtt" : "minden más";
+
+alert(k);
