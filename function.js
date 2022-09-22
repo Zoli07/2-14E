135f09a82a809e895d67ece86c472968a9b90fbd
Borbély Zoltán <borbelyz@kkszki.hu>
2022-09-22 17:50:42 +0200

    óra2

--- function.js
+++ function.js
@@ -4,7 +4,7 @@
 let func=Hello;
 func();
 Hello();
-*/
+
 let hello=function(){
     console.log("hello");
 }
@@ -14,3 +14,24 @@ function sum(a,b){
     return a+b;
 }
 console.log(sum(2,3));
+
+let osszeg=(a,b) => a+b;
+alert(osszeg(1,2));
+
+
+let double =n =>n*2;
+console.log(double(3));
+
+let age=prompt("Hány éves vagy? ");
+let hello =(age<18) ?
+()=> alert("Hello"):
+()=> alert("Üdvözlöm!");
+
+hello();
+*/
+let osszeg= (a,b) => {
+    let eredmeny=a+b;
+    console.log("hello")
+    return eredmeny;
+}
+console.log(osszeg(1,2));
